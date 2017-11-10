22e64bae5673a42470eb7a49fda99ceabbf36d68
tomphan <phanthaitrung@gmail.com>
2017-11-10 17:59:13 +0700

    Commit some

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,7 +26,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,18 +3,52 @@ import {  ModalController } from 'ionic-angular';
 
 import { NewPostPage } from './new-post/new-post';
 
+var database_json = '/database.json';
+
+//var database_json = 'http://nguoilamnong.vn/mxh/json';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public modalCtrl: ModalController) { }
+export class HomePage implements OnInit {
+
+  cards;
+  data_results: string[];
+
+  //constructor(public modalCtrl: ModalController) {     
+  constructor(private http: HttpClient) {               // Inject HttpClient into your component or service.
+  }
   
   newStatusPost(characterNum) {
     let modal = this.modalCtrl.create(NewPostPage, characterNum);
     //let modal = this.modalCtrl.create(ModalContentPage, characterNum);
     modal.present(); 
+  }
+  
+  ngOnInit(): void {
+    // Make the HTTP request:
+    this.http
+    .get<ItemsResponse>('database_json')
+    .retry(3)
+    .subscribe(
+      data => {
+        // Read the result field from the JSON response.
+        this.data_results = data['results'];
+        this.cards = this.data_results.posts;
+      },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          // A client-side or network error occurred. Handle it accordingly.
+          console.log('An error occurred:', err.error.message);
+        } else {
+          // The backend returned an unsuccessful response code.
+          // The response body may contain clues as to what went wrong,
+          console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+        }
+      }
+    );
   }
 
 }
