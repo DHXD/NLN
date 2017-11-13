171c92c104b2a5600386b6e4fb6ac21f635a61c1
tomphan <phanthaitrung@gmail.com>
2017-11-13 13:02:14 +0700

    Them file farm-iot-dieu-khien

---
+++ src/pages/farm/farm-iot-dieu-khien.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+
+
+import {
+  MqttMessage,
+  MqttModule,
+  MqttService
+} from 'ngx-mqtt';
+
+@Component({
+  selector: 'page-notify',
+  templateUrl: 'notify.html'
+})
+
+export class NotifyPage {
+  myMessage;
+    public myOtherMessage$: Observable<MqttMessage>;
+    
+    constructor(private _mqttService: MqttService) {
+      this._mqttService.observe('mqtt-out').subscribe((message: MqttMessage) => {
+        this.myMessage = message.payload.toString();
+      });
+      this.myOtherMessage$ = this._mqttService.observe('mqtt-out');
+    }
+  
+    public unsafePublish(topic: string, message: string): void {
+      this._mqttService.unsafePublish(topic, message, {qos: 1, retain: true});
+    }
+}
+
+
+
